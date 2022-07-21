ff9ed5e6a09c1bc2a743290dd0afdbe10de418c1
Lasith9812 <lasiththilina9812@gmail.com>
2022-07-21 18:31:05 +0530

    Add start and End numbers

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os
+from numpy import size
+
+start = int(input("Start Number: "))
+end = int(input("End Number: "))
 
 r = os.listdir(".")
+cc = input("Split characters by: ")
 
-print(r)
-
+print("Number of files in the directory:"+ str(size(r)))
 r.remove("main.py")
-
-print(r)
-
-for i in r:
-    
-    os.rename(i,)
+try:
+    for i in r:
+        ext = str(i.split('.',-1)[1])
+        k = (i.split(str(cc)))
+        for j in k:
+            for l in range(start,end):
+                if j == str(l):
+                    os.rename(str(i),j+"."+ext)
+except:
+    pass
